787db4d96bca00b6460536f82fc93ff2c40f5f53
Daniel Gasparik <daniel.gasparik1@gmail.com>
2020-01-29 19:17:16 +0100

    fixed descending bubble sort so it works

--- main.c
+++ main.c
@@ -1,48 +1,60 @@
 #include <stdio.h>
 #include <math.h>
-int ascendingBubbleSort(int *arr,int n){
-int i=0,j=0,temporary;
-for(i;i<n;i++){
-    for(j;j<n-1;j++){
-        if(arr[i]>arr[j]){
-            arr[i]=temporary;
-            arr[i]=arr[j];
-            arr[j]=temporary;
+int descendingBubbleSort(int arr[],int n){//written using pointers
+int temporary;
+int *p,*q;
+for(p=arr;p<arr+n;p++){
+    for(q=arr;q<arr+n-1;q++){
+        if(*p>*q){
+            temporary=*p;
+            *p=*q;
+            *q=temporary;
 
 
         }
     }
 }
-printf("Worst case complexity is O(n^2)=%d\n",pow(n,2));
-printf("Bubble sorted array is: [");
-for(i=0;i<n;i++){
-    printf("%d ",arr[i]);
+printf("Worst case complexity is O(n^2)=%.0f\n",pow(n,2));
+printf("Bubble sorted array is: [ ");
+for(p=arr;p<arr+n;p++){
+    printf("%d,",*p);
 }
 printf("]");
+return 0;
+}
+int ascendingBubbleSort(int arr[],n){//written using indexes
+    int i,j;
+    int temporary;
+    for(i=0;i<n;i++){
+        if(*arr_ptr<*(arr_ptr+1)){
+            *arr_ptr=temporary;
+
+        }
+
+    }
 
 }
-int descendingBubbleSort(int *arr,n){
-
-}
+ */
 int main() {
     int input,end,n;
     //how many numbers in array
-    printf("Decide how many numbers I will be sorting");
-    scanf("\n%d",&n);
+    printf("Decide how many numbers I will be sorting\n");
+    scanf("%d",&n);
     int arr[n];
     //load them into an array
     for(int i=0;i<n;i++){
         scanf("%d",&arr[i]);
     }
-    while((input=getchar()!='e'))
-    switch(input){
-        case 'a':
-            ascendingBubbleSort(arr,n);
-            break;
-        case 'd':
-            descendingBubbleSort(arr,n);
-            break;
+    while((input=getchar())!='e') {
+        switch (input) {
+            case 'a':
+                //ascendingBubbleSort(arr, n);
+                break;
+            case 'd':
+                descendingBubbleSort(arr,n);
+                break;
 
+        }
     }
     return 0;
 }
